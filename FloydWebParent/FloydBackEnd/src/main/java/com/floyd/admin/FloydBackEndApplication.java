b69922baa6e3789d7ed03f5dc641824db4a70fda
JaehyeonPaak <wogus392@gmail.com>
2022-09-07 21:09:52 +0900

    initial

---
+++ FloydWebParent/FloydBackEnd/src/main/java/com/floyd/admin/FloydBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.floyd.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloydBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloydBackEndApplication.class, args);
+	}
+
+}
