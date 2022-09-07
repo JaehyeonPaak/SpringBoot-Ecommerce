838edd1167be8d6976fc9d510504c85b5fd67fd7
JaehyeonPaak <wogus392@gmail.com>
2022-09-08 00:06:54 +0900

    .

---
+++ FloydWebParent/FloydFrontEnd/src/main/java/com/floyd/site/FloydFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.floyd.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloydFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloydFrontEndApplication.class, args);
+	}
+
+}
---
+++ FloydWebParent/FloydFrontEnd/src/test/java/com/floyd/site/FloydFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floyd.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloydFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
