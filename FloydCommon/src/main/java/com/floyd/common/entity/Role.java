01d9af9cfb88c1bcfc1e440c813a2463b838c7a0
JaehyeonPaak <wogus392@gmail.com>
2022-09-08 21:18:05 +0900

    Created Role entity

---
+++ FloydCommon/src/main/java/com/floyd/common/entity/Role.java
@@ -0,0 +1,25 @@
+package com.floyd.common.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 150, nullable = false)
+    private String description;
+}
