52667fd997c44b9bb1da9c0f23d385636f5a7f46
JaehyeonPaak <wogus392@gmail.com>
2022-09-12 13:22:07 +0900

    New User Form Page

--- FloydWebParent/FloydBackEnd/src/main/java/com/floyd/admin/user/UserController.java
+++ FloydWebParent/FloydBackEnd/src/main/java/com/floyd/admin/user/UserController.java
@@ -1,9 +1,11 @@
 package com.floyd.admin.user;
 
+import com.floyd.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -13,8 +15,27 @@ public class UserController {
 
     @GetMapping("/users")
     public String listAll(Model model) {
-        var listUsers = userService.listAll();
+        var listUsers = userService.listUsers();
         model.addAttribute("listUsers", listUsers);
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String createNewUser(Model model) {
+        User user = new User();
+        user.setEnabled(true);
+
+        var roles = userService.listRoles();
+
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveNewUser(User user) {
+        userService.save(user);
+        return "redirect:/users";
     }
 }
--- FloydWebParent/FloydBackEnd/src/main/java/com/floyd/admin/user/UserService.java
+++ FloydWebParent/FloydBackEnd/src/main/java/com/floyd/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.floyd.admin.user;
 
+import com.floyd.common.entity.Role;
 import com.floyd.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> listAll() {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public List<User> listUsers() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
     }
 }
