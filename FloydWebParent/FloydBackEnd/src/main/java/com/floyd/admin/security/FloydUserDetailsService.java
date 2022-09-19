7809f972241737e8bd3164b60fb54020ab443f0d
JaehyeonPaak <wogus392@gmail.com>
2022-09-19 15:48:14 +0900

    User Authentication

---
+++ FloydWebParent/FloydBackEnd/src/main/java/com/floyd/admin/security/FloydUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.floyd.admin.security;
+
+import com.floyd.admin.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class FloydUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.getUserByEmail(email);
+        if (user != null) {
+            return new FloydUserDetails(user);
+        }
+        throw new UsernameNotFoundException("Could not find user with email: " + email);
+    }
+}
